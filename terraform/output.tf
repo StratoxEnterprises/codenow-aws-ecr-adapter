a35aef8c613c933052aa573adac3184d11b94d9d
Romanecek <roman.furst@stratox.cz>
2023-07-14 11:16:24 +0200

    CNINF-979 add repository writer to output & fix default helm values

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,13 @@
 output "container_registry_repositories" {
   value = module.ecr.*.repositories
 }
+
+output "container_registry_user_writer_username" {
+  value = module.ecr.*.repository_user_writer_username
+  sensitive = true
+}
+
+output "container_registry_user_writer_password" {
+  value = module.ecr.*.repository_user_writer_password
+  sensitive = true
+}
